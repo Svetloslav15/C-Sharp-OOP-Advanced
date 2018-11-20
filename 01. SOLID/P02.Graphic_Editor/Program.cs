1a2f4474e37d2ba6762d20a4eeb3a99e4f5dc1f0
Svetloslav15 <sn_novoselski@abv.bg>
2018-11-20 23:03:54 +0200

    add solid

---
+++ 01. SOLID/P02.Graphic_Editor/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace P02.Graphic_Editor
+{
+    class Program
+    {
+        static void Main()
+        {
+            List<IShape> shapes = new List<IShape>();
+            shapes.Add(new Rectangle());
+            shapes.Add(new Circle());
+            GraphicEditor graphicEditor = new GraphicEditor();
+            foreach (var item in shapes)
+            {
+                graphicEditor.DrawShape(item);
+            }
+        }
+    }
+}
